6cac06d74c5edba11b056c121a5d69493a738c45
AYISHA YS <84435965+ayishays83@users.noreply.github.com>
2023-08-26 14:06:10 +0530

    frontend
    addded frontend files

---
+++ signup-frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import SignUpForm from './components/SignUpForm';
+
+const App = () => {
+  return (
+    <div className="App">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default App;
+
